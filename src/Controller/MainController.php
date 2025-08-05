4ae57504b4363267d590fe378bfb20147ed1168b
oksana-1 <4438488@gmail.com>
2025-08-05 22:11:56 +0300

    Add php-cs-fixer, fix styles

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,10 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController
 {
     #[Route('/')]
-
     public function homepage(): Response
     {
         return new Response('<div>Hello, world!</div>');
     }
 }
-
